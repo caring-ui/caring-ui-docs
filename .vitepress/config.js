70529e25738e5e9d98c1e55b4093bb95fa8d9c30
风度万人迷 <805391114@qq.com>
2024-05-08 17:52:37 +0800

    fix:editLink

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,7 +2,7 @@
  * @Author: Wanko
  * @Date: 2024-04-30 14:41:18
  * @LastEditors: Wanko
- * @LastEditTime: 2024-05-08 17:47:07
+ * @LastEditTime: 2024-05-08 17:52:04
  * @Description:
  */
 import { defineConfig } from 'vitepress'
@@ -20,7 +20,7 @@ export default defineConfig({
     logo: '/logo.png',
     editLink: {
       pattern:
-        'https://github.com/Fascinating-W/vitepress/edit/master/docs/:path',
+        'https://github.com/caring-ui/caring-ui-docs/edit/master/docs/:path',
       text: '在 GitHub 上编辑此页面'
     },
     outlineTitle: '大纲',
@@ -67,7 +67,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'github', link: 'https://github.com/caring-ui/caring-ui-docs' },
       {
         icon: {
           svg: '<svg t="1714560896104" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4243" width="200" height="200"><path d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z" fill="#C71D23" p-id="4244"></path></svg>'
